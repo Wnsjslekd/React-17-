c7dd06d59ac58853f7d47cf46a0ab3aa28ccf37b
이준원 <ljw1205317@naver.com>
2026-02-26 14:49:28 +0900

    React 17일차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import {lazy, Suspense} from "react";
 import "./App.css";
-import TestComponent from "./TestComponent";
+
+const LazyComponent = lazy(() => import("./TestComponent"))
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
         lazy와 Suspense
         <br /> 사용해보기
       </h1>
-      <TestComponent />
+      <Suspense fallback={<h2>로딩중</h2>}>
+        <LazyComponent />
+      </Suspense>
     </div>
   );
 }
